1c48ddaac91b5df056e2f034cfba5eab52092899
Mohammad Ghasemian <ghasemianmohammad2@gmail.com>
2026-06-20 12:11:29 +0330

    feat(auth):create login page #1

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <input type="email" placeholder="Email" required />
+      <input type="password" placeholder="Password" required />
+
+      <button>Login</button>
+    </div>
+  );
+}
